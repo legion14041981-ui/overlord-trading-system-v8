263c81df961b2f271596d7b281e34e724faa4ed4
legion14041981-ui <legion14041981@gmail.com>
2026-01-01 22:19:43 +0300

    feat(schemas): initialize schemas package structure (Phase 19)

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,16 +1,18 @@
-# Pydantic schemas export
-from .user import UserCreate, UserUpdate, UserResponse
-from .strategy import StrategyCreate, StrategyUpdate, StrategyResponse
-from .trade import TradeCreate, TradeUpdate, TradeResponse
+# src/schemas/__init__.py
+"""
+Pydantic Schemas Package
+Пакет Pydantic схем - валидация API реквестов и ответов
+"""
+
+# Schemas will be imported here as they are created
+# from src.schemas.exchange import ExchangeConfigSchema, TickerSchema, OrderSchema
+# from src.schemas.portfolio import PortfolioSchema, PositionSchema
+# from src.schemas.trade import TradeSchema
+# from src.schemas.strategy import StrategySchema
+# from src.schemas.market_data import MarketDataSchema
+# from src.schemas.alert import AlertSchema
+# from src.schemas.user import UserSchema
 
 __all__ = [
-    "UserCreate",
-    "UserUpdate",
-    "UserResponse",
-    "StrategyCreate",
-    "StrategyUpdate",
-    "StrategyResponse",
-    "TradeCreate",
-    "TradeUpdate",
-    "TradeResponse",
+    # Schemas (to be populated)
 ]
