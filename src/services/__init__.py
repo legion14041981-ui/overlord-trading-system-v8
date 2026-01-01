f454204ba713b5d30dd5d6681bcd22c90340c448
legion14041981-ui <legion14041981@gmail.com>
2026-01-01 22:32:42 +0300

    feat: Add services module init with all service exports

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,23 +1,18 @@
-# src/services/__init__.py
 """
-Business Logic Services Package
-Слой бизнес-логики - сервисы для управления трейдингом и портфолем
+Services Module for Overlord v8.1 Trading System
+
+Core business logic services for:
+- Market data management and analysis
+- Alert rule creation and triggering
+- Trade execution
+- Portfolio management
+- Risk management
 """
 
-# Services will be imported here as they are created
-# from src.services.exchange_service import ExchangeService
-# from src.services.portfolio_service import PortfolioService
-# from src.services.risk_service import RiskManagementService
-# from src.services.strategy_service import StrategyService
-# from src.services.market_data_service import MarketDataService
-# from src.services.alert_service import AlertService
+from src.services.market_data_service import MarketDataService
+from src.services.alert_service import AlertService
 
 __all__ = [
-    # Services (to be populated)
-    # "ExchangeService",
-    # "PortfolioService",
-    # "RiskManagementService",
-    # "StrategyService",
-    # "MarketDataService",
-    # "AlertService",
+    "MarketDataService",
+    "AlertService",
 ]
