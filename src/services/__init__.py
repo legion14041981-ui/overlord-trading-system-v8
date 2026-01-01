757754dd5682cf59fa961cc7b511f1abbb29ae24
legion14041981-ui <legion14041981@gmail.com>
2026-01-01 22:18:59 +0300

    feat(services): initialize services package structure (Phase 9)

---
+++ src/services/__init__.py
@@ -0,0 +1,23 @@
+# src/services/__init__.py
+"""
+Business Logic Services Package
+Слой бизнес-логики - сервисы для управления трейдингом и портфолем
+"""
+
+# Services will be imported here as they are created
+# from src.services.exchange_service import ExchangeService
+# from src.services.portfolio_service import PortfolioService
+# from src.services.risk_service import RiskManagementService
+# from src.services.strategy_service import StrategyService
+# from src.services.market_data_service import MarketDataService
+# from src.services.alert_service import AlertService
+
+__all__ = [
+    # Services (to be populated)
+    # "ExchangeService",
+    # "PortfolioService",
+    # "RiskManagementService",
+    # "StrategyService",
+    # "MarketDataService",
+    # "AlertService",
+]
