4fc0db90a0d98916dd3e58e515b58f98f40701f1
legion14041981-ui <legion14041981@gmail.com>
2026-01-02 15:12:28 +0300

    feat: Complete BLOCK 3 - Execution Layer with __init__.py

--- src/execution/__init__.py
+++ src/execution/__init__.py
@@ -1,14 +1,18 @@
 """Execution layer for order management and routing."""
+
 from .order_manager import OrderManager
-from .execution_engine import ExecutionEngine
-from .smart_router import SmartRouter
-from .slippage_controller import SlippageController
+from .execution_engine import ExecutionEngine, ExecutionReport
+from .smart_router import SmartRouter, RoutingDecision
+from .slippage_controller import SlippageController, SlippageMetrics
 from .position_tracker import PositionTracker
 
 __all__ = [
     'OrderManager',
     'ExecutionEngine',
+    'ExecutionReport',
     'SmartRouter',
+    'RoutingDecision',
     'SlippageController',
-    'PositionTracker'
+    'SlippageMetrics',
+    'PositionTracker',
 ]
